9f962462d27e8fd8a6f57ee00f3a5d7f32c3c554
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-02-04 23:03:38 -0600

    initial commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Anderson Cardoso on 2/4/18.
+//  Copyright © 2018 Anderson Cardoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
